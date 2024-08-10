61fccc15af1842e4ee29b763c8f87b60cf59bb9c
Aniket <aniketjadhav310@gmail.com>
2024-08-10 15:54:27 +0530

    database part added and use learn use of database

--- ChaiaurDjango/chai/urls.py
+++ ChaiaurDjango/chai/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.chai_home,name="chai_home"),
-
+    path('<int:chai_id>/',views.chai_detail,name="chai_detail"),
 ]
--- ChaiaurDjango/chai/views.py
+++ ChaiaurDjango/chai/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import ChaiVariety
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def chai_home(request):
+    chais = ChaiVariety.objects.all()
     #return HttpResponse("Hello world!! This is Home page")
-    return render(request,'chai/chai_home.html')
+    return render(request,'chai/chai_home.html',{'chais':chais})
+
+def chai_detail(request,chai_id):
+    chai=get_object_or_404(ChaiVariety,pk=chai_id)
+    #return HttpResponse("Hello world!! This is Home page")
+    return render(request,'chai/chai_detail.html',{'chai':chai})
